3bd0bddb43cb97fc5cfdad3208a15258cbfd8e77
paulafpritchett <paulafp@mrc-lmb.cam.ac.uk>
2019-10-25 11:06:11 +0100

    Adding main wrapper script.

---
+++ CAM.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+PROG_NAME = 'CAM'
+DESCRIPTION = 'CRISPR Analysis Module'
+
+# Fastq file trimming using trimgalore
+# Defaults: --length 12 -a GTTTAAGAGCTAAGCTGGAAACAGCATAGCAA
+
+# Alignment using bowtie2
+# Defaults: --no-sq -5 1 -N 1
+
+
+# Bam files processing to create input for MAGeCK
+## Remove multi-mapped reads: sed '/XS:/d'
+##
+
+

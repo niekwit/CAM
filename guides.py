3f6283388c071a4b5e959e832b9d3f04de2feadf
Niek Wit <41302859+niekwit@users.noreply.github.com>
2020-03-26 09:56:10 +0000

    writes library guide name to csv

--- guides.py
+++ guides.py
@@ -5,16 +5,17 @@ Created on Sun Mar 22 10:47:22 2020
 
 @author: Niek Wit (MRC LMB)
 
-This function returns a list of guides names extracted from the FASTA CRISPR library file
+This function returns a .csv file of guides names extracted from the FASTA CRISPR library file
 """
 
 import pandas as pd
 
 def guide_names(library):
+    library_name = library
     library = pd.read_csv(library + '.fasta', names = ['guide'])
     #creates new dataframe with only guide names:
     library = library[library['guide'].str.contains('>')]
     #removes '<' character from each row:
     library = library['guide'].str.strip('>')
-    library = library.tolist()
-    return library
+    #saves guide names to a .csv file:
+    library.to_csv(library_name + '_guides.csv', index=False)
